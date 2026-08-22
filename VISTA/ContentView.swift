88c71870fcd8f4cdab5b0884e2e0d49ee65fb060
Maliya <maliyapalanikumar@gmail.com>
2026-08-22 13:17:20 +0530

    Initial Commit

---
+++ VISTA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VISTA
+//
+//  Created by Maliya Palanikumar on 22/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VISTA/VISTAApp.swift
@@ -0,0 +1,17 @@
+//
+//  VISTAApp.swift
+//  VISTA
+//
+//  Created by Maliya Palanikumar on 22/08/26.
+//
+
+import SwiftUI
+
+@main
+struct VISTAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
